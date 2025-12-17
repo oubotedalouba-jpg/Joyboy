e84bbc1568b3d0722b7a9294424b6d81b3a01514
oubotedalouba-jpg <oubotedalouba@gmail.com>
2025-12-17 12:16:52 +0000

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,10 @@
+//=====🏅 ᴍᴀʟᴠɪɴ  xᴅ🔥====
+
+module.exports = {
+  BOT_NAME: 'ᴍᴀʟᴠɪɴ xᴅ', // ur bot name
+  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
+  OWNER_NUMBER: '263714757857', // add your phone number
+  SESSION_ID: '', // add your session ID
+  MODE: "public", // public or private
+  TIMEZONE: 'Africa/Harare' // ur timezone
+};
