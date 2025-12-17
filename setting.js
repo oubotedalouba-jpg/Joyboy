356d217a430bcd46eed3743977b3c3024fe623ef
oubotedalouba-jpg <oubotedalouba@gmail.com>
2025-12-17 12:18:46 +0000

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,9 +1,9 @@
 //=====🏅 ᴍᴀʟᴠɪɴ  xᴅ🔥====
 
 module.exports = {
-  BOT_NAME: 'ᴍᴀʟᴠɪɴ xᴅ', // ur bot name
-  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
-  OWNER_NUMBER: '263714757857', // add your phone number
+  BOT_NAME: 'JOYBOY xᴅ', // ur bot name
+  OWNER_NAME: 'JOY BOY', //your name
+  OWNER_NUMBER: '22896290507', // add your phone number
   SESSION_ID: '', // add your session ID
   MODE: "public", // public or private
   TIMEZONE: 'Africa/Harare' // ur timezone
